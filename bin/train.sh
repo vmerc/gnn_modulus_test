ed49a59c121a9c92efe8cc8a3d31ffd631b8156a
Valentin Mercier <valentinmercier@MacBook-Pro-de-Valentin.local>
2024-06-18 12:18:49 +0200

    preparation des premiers tests turpan

--- bin/train.sh
+++ bin/train.sh
@@ -8,4 +8,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 
+export MASTER_PORT=$(echo "${SLURM_JOB_ID} % 100000 % 50000 + 30000" | bc)
+export MASTER_ADDR=$(hostname --ip-address)
+echo "MASTER_ADDR:MASTER_PORT="${MASTER_ADDR}:${MASTER_PORT}
+
 apptainer exec --bind /tmpdir,/work --nv /work/conteneurs/sessions-interactives/modulus-24.04-calmip-si.sif python train_script.py
