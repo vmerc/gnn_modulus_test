72456cf7d37844bcec249019147122fae0ebdf4f
valentin.0111@hotmail.fr <valentin.mercier@etu.enseeiht.fr>
2024-07-02 10:29:37 +0200

    multimesh creation

---
+++ bin/create_multimesh.py
@@ -0,0 +1,25 @@
+import numpy as np 
+import os 
+import sys
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', ''))
+sys.path.append(project_path)
+
+from python.create_dgl_dataset import TelemacDataset,create_multimesh
+from python.python_code.data_manip.extraction.telemac_file import TelemacFile
+
+import torch
+from tqdm import trange
+import copy
+import pandas as pd
+import random
+import dgl
+
+res_list = ['/projets/aniti-daml/vmercier/simu_valentin/TetQ2500_intermediaire_bdd.res']
+mesh_list = ['/projets/aniti-daml/vmercier/simu_valentin/maillage_3.slf']
+cli_list = ['/projets/aniti-daml/vmercier/simu_valentin/cli']
+coarse_mesh_list = ['./data/maillage_grossier.slf']
+
+data_folder = './data/TetQ2500inter_1min_chunk/'
+dataset_name = 'TetQ2500inter_1min'
+
+create_multimesh(mesh_list[0],coarse_mesh_list,res_list,cli_list,data_folder,dataset_name)
